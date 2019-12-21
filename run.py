c2855fd56eb3ee2a069ee6152f1beaa080c90d28
chisolum <co97@njit.edu>
2019-12-20 21:29:02 -0500

    Task : Complete part 3

--- run.py
+++ run.py
@@ -1,18 +1,11 @@
 # /run.py
 import os
-
 from blog_api.src.app import create_app
 
-
-def set_environment_variable():
-    os.environ["FLASK_ENV"] = "development"
-    os.environ["DATABASE_URL"] = 'postgres://name:password@houst:port/blog_api_db'
-    os.environ["JWT_SECRET_KEY"] = 'hhgaghhgsdhdhdd'
-
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
 if __name__ == '__main__':
-    set_environment_variable()
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
-# run app
-    app.run()
+    port = os.getenv('PORT')
+    # run app
+app.run(host='0.0.0.0', port=port)
